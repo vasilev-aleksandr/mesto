19c44472d7f734871876b0e3451080a669a1e477
Aleksandr Vasilev <sovietxyouth@gmail.com>
2020-12-22 18:47:54 +0300

    take two

--- sripts/index.js
+++ sripts/index.js
@@ -1,49 +1,42 @@
 let editButton = document.querySelector('.profile__edit-button')
 let popup = document.querySelector('.popup')
 let closeButton = document.querySelector('.popup__close-button')
+let form = document.querySelector('.popup__form')
+let likeButtons = document.querySelectorAll('.place__like')
+
+let nameInput = document.querySelector('.popup__input_name')
+let jobInput = document.querySelector('.popup__input_about')
+let profileName = document.querySelector('.profile__name')
+let profileAbout = document.querySelector('.profile__about')
+
+function removePopupActiveClass() {
+  popup.classList.remove('popup_active')
+}
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+  profileName.textContent = nameInput.value
+  profileAbout.textContent = jobInput.value
+  removePopupActiveClass()
+}
 
 editButton.addEventListener ('click', () => {
-  popup.classList.toggle('popup_active')
-  let nameInput = document.querySelector('.popup__name')
-  let jobInput = document.querySelector('.popup__about')
-  let profileName = document.querySelector('.profile__name')
-  let profileAbout = document.querySelector('.profile__about')
-  nameInput.value = profileName.textContent 
+  popup.classList.add('popup_active')
+  nameInput.value = profileName.textContent
   jobInput.value = profileAbout.textContent
 })
 
-closeButton.addEventListener ('click', () => {
-  popup.classList.toggle('popup_active')
-})
-
-popup.addEventListener ('click', (event) => {
-  if (event.target === event.currentTarget)
-  popup.classList.remove('popup_active')
-})
-
-let formElement = document.querySelector('.popup__form')
-
-function handleFormSubmit (evt) {
-  evt.preventDefault();
-  let nameInput = document.querySelector('.popup__name')
-  let jobInput = document.querySelector('.popup__about')
-  let profileName = document.querySelector('.profile__name')
-  let profileAbout = document.querySelector('.profile__about')
-  profileName.textContent = nameInput.value 
-  profileAbout.textContent = jobInput.value
-  popup.classList.remove('popup_active')
-}
-let submitButton = document.querySelector('.popup__save-button')
-submitButton.addEventListener('click', handleFormSubmit)
-
-let form = document.querySelector('.popup__form')
 form.addEventListener('submit', handleFormSubmit)
 
+closeButton.addEventListener('click', removePopupActiveClass)
 
-let likeButtons = document.querySelectorAll('.place__like')
+popup.addEventListener('click', (event) => {
+  if (event.target === event.currentTarget)
+    removePopupActiveClass()
+})
 
 for (let i = 0; i < likeButtons.length; i++) {
-likeButtons[i].addEventListener('click', () => {
-likeButtons[i].classList.toggle('place__like_active')
-})
+  likeButtons[i].addEventListener('click', () => {
+    likeButtons[i].classList.toggle('place__like_active')
+  })
 }
