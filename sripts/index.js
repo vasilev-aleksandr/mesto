8d2ab131ad40278d3021c0a5c0eea35b4631302a
Aleksandr Vasilev <sovietxyouth@gmail.com>
2021-01-19 20:59:53 +0300

    fixed script

--- sripts/index.js
+++ sripts/index.js
@@ -1,6 +1,32 @@
+// общие функции
+
+function removePopupActiveClass(popup) {
+  popup.classList.remove('popup_active')
+}
+
+function addPopupActiveClass(popup) {
+  popup.classList.add('popup_active')
+}
+
+function addPopupCloseListeners(popups) {
+  popups.forEach(popup => {
+    const closeButton = popup.querySelector('.popup__close-button')
+    closeButton.addEventListener('click', () => removePopupActiveClass(popup))
+
+    popup.addEventListener('click', (event) => {
+      if (event.target === event.currentTarget) removePopupActiveClass(popup)
+    })
+  })
+}
+
+const pagePopups = document.querySelectorAll('.popup')
+addPopupCloseListeners(pagePopups)
+
+
+// редактирование профиля
+
 const editButton = document.querySelector('.profile__edit-button')
 const popupProfile = document.querySelector('.popup_profile')
-const profilePopupCloseButton = popupProfile.querySelector('.popup__close-button')
 const form = document.querySelector('.popup__form')
 const nameInput = document.querySelector('.popup__input_profile_name')
 const jobInput = document.querySelector('.popup__input_profile_about')
@@ -8,56 +34,24 @@ const profileName = document.querySelector('.profile__name')
 const profileAbout = document.querySelector('.profile__about')
 
 function handleEditButtonClick() {
-  popupProfile.classList.add('popup_active')
+  addPopupActiveClass(popupProfile)
   nameInput.value = profileName.textContent
   jobInput.value = profileAbout.textContent
 }
 
-function removePopupProfileActiveClass() {
-  popupProfile.classList.remove('popup_active')
-}
-
 function handleFormSubmit(event) {
-  event.preventDefault();
+  event.preventDefault()
   profileName.textContent = nameInput.value
   profileAbout.textContent = jobInput.value
-  removePopupProfileActiveClass()
+  removePopupActiveClass(popupProfile)
 }
 
 editButton.addEventListener('click', handleEditButtonClick)
 
 form.addEventListener('submit', handleFormSubmit)
 
-profilePopupCloseButton.addEventListener('click', removePopupProfileActiveClass)
 
-popupProfile.addEventListener('click', (event) => {
-  if (event.target === event.currentTarget)
-  removePopupProfileActiveClass()
-})
-
-
-
-const addButton = document.querySelector('.profile__add-button')
-const popupPlace = document.querySelector('.popup_place')
-const placePopupCloseButton = popupPlace.querySelector('.popup__close-button')
-
-function handleAddButtonClick() {
-popupPlace.classList.add('popup_active')
-}
-
-addButton.addEventListener('click', handleAddButtonClick)
-
-function removePopupPlaceActiveClass() {
-  popupPlace.classList.remove('popup_active')
-}
-
-placePopupCloseButton.addEventListener('click', removePopupPlaceActiveClass)
-
-popupPlace.addEventListener('click', (event) => {
-  if (event.target === event.currentTarget)
-    removePopupPlaceActiveClass()
-})
-
+// работа с карточками
 const initialCards = [
   {
     name: 'Архыз',
@@ -83,77 +77,65 @@ const initialCards = [
     name: 'Байкал',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
   }
-]; 
+]
 
 initialCards.reverse()
 
-
 const places = document.querySelector('.places')
 const placeTemplate = document.querySelector('#place-template')
+const addButton = document.querySelector('.profile__add-button')
+const popupPlace = document.querySelector('.popup_place')
 const placeForm = popupPlace.querySelector('.popup__form')
 const popupImage = document.querySelector('.popup_image')
 
-
-
-
-function addPlace(name,link){
+function addPlace(name,link) {
   const placeElement = placeTemplate.content.cloneNode(true)
   const placeHeading = placeElement.querySelector('.place__heading')
-  placeHeading.textContent = name;
+  placeHeading.textContent = name
+
   const placePhoto = placeElement.querySelector('.place__photo')
-  placePhoto.src = link;
+  placePhoto.src = link
   placePhoto.addEventListener('click', () => handleImageZoomInClick(name, link))
+
   const placeDeleteButton = placeElement.querySelector('.place__delete-button')
   placeDeleteButton.addEventListener('click', handlePlaceDelete)
+
   const placeLikeButton = placeElement.querySelector('.place__like')
   placeLikeButton.addEventListener('click', function (evt) {
-  evt.target.classList.toggle('place__like_active')
+    evt.target.classList.toggle('place__like_active')
   })
+
   places.prepend(placeElement)
-  }
+}
 
 initialCards.forEach(card => addPlace(card.name, card.link))
 
+function handleAddButtonClick() {
+  addPopupActiveClass(popupPlace)
+}
 
-  function handlePlaceSubmit(event) {
-    event.preventDefault()
-    const headingInput = document.querySelector('.popup__input_place_name')
-    const linkInput = document.querySelector('.popup__input_place_link')
-    addPlace(headingInput.value, linkInput.value)
-    headingInput.value = ''
-    linkInput.value = ''
-    removePopupPlaceActiveClass()
-    }
+addButton.addEventListener('click', handleAddButtonClick)
 
-  placeForm.addEventListener('submit', handlePlaceSubmit)
+function handlePlaceSubmit(event) {
+  event.preventDefault()
 
-  function handlePlaceDelete(evt){
-    evt.target.closest('.place').remove()
-  }
+  const headingInput = document.querySelector('.popup__input_place_name')
+  const linkInput = document.querySelector('.popup__input_place_link')
+  addPlace(headingInput.value, linkInput.value)
 
-  function handleImageZoomInClick(imageHeading, imageSource){
-    popupImage.classList.add('popup_active')
-    popupImage.querySelector('.popup__image-photo').src = imageSource;
-    popupImage.querySelector('.popup__image-description').textContent = imageHeading;
-  }
+  headingInput.value = ''
+  linkInput.value = ''
+  removePopupActiveClass(popupPlace)
+}
 
-  const imagePopupCloseButton = popupImage.querySelector('.popup__close-button')
+placeForm.addEventListener('submit', handlePlaceSubmit)
 
-  function removePopupImageActiveClass() {
-    popupImage.classList.remove('popup_active')
-  }
-  
-  imagePopupCloseButton.addEventListener('click', removePopupImageActiveClass)
+function handlePlaceDelete(evt) {
+  evt.target.closest('.place').remove()
+}
 
-  popupImage.addEventListener('click', (event) => {
-    if (event.target === event.currentTarget)
-    removePopupImageActiveClass()
-  })
-  
-
-
-
-
-
-  
-
+function handleImageZoomInClick(imageHeading, imageSource) {
+  addPopupActiveClass(popupImage)
+  popupImage.querySelector('.popup__image-photo').src = imageSource
+  popupImage.querySelector('.popup__image-description').textContent = imageHeading
+}
