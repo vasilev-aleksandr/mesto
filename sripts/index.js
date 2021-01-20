0a9e47334727bf7f42e9e171da8469408ca6f58e
Aleksandr Vasilev <sovietxyouth@gmail.com>
2021-01-20 22:15:04 +0300

    fixed bugs

--- sripts/index.js
+++ sripts/index.js
@@ -1,57 +1,22 @@
-// общие функции
+const editButton = document.querySelector('.profile__edit-button');
+const popupProfile = document.querySelector('.popup_profile');
+const form = document.querySelector('.popup__form');
+const nameInput = document.querySelector('.popup__input_profile_name');
+const jobInput = document.querySelector('.popup__input_profile_about');
+const profileName = document.querySelector('.profile__name');
+const profileAbout = document.querySelector('.profile__about');
+const places = document.querySelector('.places');
+const placeTemplate = document.querySelector('#place-template');
+const addButton = document.querySelector('.profile__add-button');
+const popupPlace = document.querySelector('.popup_place');
+const placeForm = popupPlace.querySelector('.popup__form');
+const headingInput = document.querySelector('.popup__input_place_name');
+const linkInput = document.querySelector('.popup__input_place_link');
+const popupImage = document.querySelector('.popup_image');
+const imagePhoto = popupImage.querySelector('.popup__image-photo');
+const imageDescription = popupImage.querySelector('.popup__image-description');
+const pagePopups = document.querySelectorAll('.popup');
 
-function removePopupActiveClass(popup) {
-  popup.classList.remove('popup_active')
-}
-
-function addPopupActiveClass(popup) {
-  popup.classList.add('popup_active')
-}
-
-function addPopupCloseListeners(popups) {
-  popups.forEach(popup => {
-    const closeButton = popup.querySelector('.popup__close-button')
-    closeButton.addEventListener('click', () => removePopupActiveClass(popup))
-
-    popup.addEventListener('click', (event) => {
-      if (event.target === event.currentTarget) removePopupActiveClass(popup)
-    })
-  })
-}
-
-const pagePopups = document.querySelectorAll('.popup')
-addPopupCloseListeners(pagePopups)
-
-
-// редактирование профиля
-
-const editButton = document.querySelector('.profile__edit-button')
-const popupProfile = document.querySelector('.popup_profile')
-const form = document.querySelector('.popup__form')
-const nameInput = document.querySelector('.popup__input_profile_name')
-const jobInput = document.querySelector('.popup__input_profile_about')
-const profileName = document.querySelector('.profile__name')
-const profileAbout = document.querySelector('.profile__about')
-
-function handleEditButtonClick() {
-  addPopupActiveClass(popupProfile)
-  nameInput.value = profileName.textContent
-  jobInput.value = profileAbout.textContent
-}
-
-function handleFormSubmit(event) {
-  event.preventDefault()
-  profileName.textContent = nameInput.value
-  profileAbout.textContent = jobInput.value
-  removePopupActiveClass(popupProfile)
-}
-
-editButton.addEventListener('click', handleEditButtonClick)
-
-form.addEventListener('submit', handleFormSubmit)
-
-
-// работа с карточками
 const initialCards = [
   {
     name: 'Архыз',
@@ -79,16 +44,43 @@ const initialCards = [
   }
 ]
 
-initialCards.reverse()
+function closePopup(popup) {
+  popup.classList.remove('popup_active');
+}
 
-const places = document.querySelector('.places')
-const placeTemplate = document.querySelector('#place-template')
-const addButton = document.querySelector('.profile__add-button')
-const popupPlace = document.querySelector('.popup_place')
-const placeForm = popupPlace.querySelector('.popup__form')
-const popupImage = document.querySelector('.popup_image')
+function openPopup(popup) {
+  popup.classList.add('popup_active');
+}
 
-function addPlace(name,link) {
+function addPopupCloseListeners(popups) {
+  popups.forEach(popup => {
+    const closeButton = popup.querySelector('.popup__close-button');
+    closeButton.addEventListener('click', () => closePopup(popup));
+
+    popup.addEventListener('click', (event) => {
+      if (event.target === event.currentTarget) closePopup(popup);
+    })
+  })
+}
+
+function handleEditButtonClick() {
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileAbout.textContent;
+  openPopup(popupProfile);
+}
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileAbout.textContent = jobInput.value;
+  removePopupActiveClass(popupProfile);
+}
+
+function handleLikeButtonClick(evt) {
+  evt.target.classList.toggle('place__like_active')
+}
+
+function createPlaceCard(name,link) {
   const placeElement = placeTemplate.content.cloneNode(true)
   const placeHeading = placeElement.querySelector('.place__heading')
   placeHeading.textContent = name
@@ -101,41 +93,42 @@ function addPlace(name,link) {
   placeDeleteButton.addEventListener('click', handlePlaceDelete)
 
   const placeLikeButton = placeElement.querySelector('.place__like')
-  placeLikeButton.addEventListener('click', function (evt) {
-    evt.target.classList.toggle('place__like_active')
-  })
+  placeLikeButton.addEventListener('click', handleLikeButtonClick)
 
-  places.prepend(placeElement)
+  return placeElement
 }
 
-initialCards.forEach(card => addPlace(card.name, card.link))
-
-function handleAddButtonClick() {
-  addPopupActiveClass(popupPlace)
+function addPlace(name,link) {
+  const card = createPlaceCard(name,link)
+  places.prepend(card)
 }
-
-addButton.addEventListener('click', handleAddButtonClick)
 
 function handlePlaceSubmit(event) {
   event.preventDefault()
-
-  const headingInput = document.querySelector('.popup__input_place_name')
-  const linkInput = document.querySelector('.popup__input_place_link')
   addPlace(headingInput.value, linkInput.value)
-
   headingInput.value = ''
   linkInput.value = ''
-  removePopupActiveClass(popupPlace)
+  closePopup(popupPlace)
 }
-
-placeForm.addEventListener('submit', handlePlaceSubmit)
 
 function handlePlaceDelete(evt) {
   evt.target.closest('.place').remove()
 }
 
 function handleImageZoomInClick(imageHeading, imageSource) {
-  addPopupActiveClass(popupImage)
-  popupImage.querySelector('.popup__image-photo').src = imageSource
-  popupImage.querySelector('.popup__image-description').textContent = imageHeading
+  openPopup(popupImage)
+  imagePhoto.src = imageSource
+  imageDescription.textContent = imageHeading
 }
+
+initialCards.reverse().forEach(card => addPlace(card.name, card.link))
+
+addPopupCloseListeners(pagePopups);
+
+editButton.addEventListener('click', handleEditButtonClick);
+
+form.addEventListener('submit', handleFormSubmit);
+
+addButton.addEventListener('click', () => openPopup(popupPlace));
+
+placeForm.addEventListener('submit', handlePlaceSubmit)
